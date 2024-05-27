9395bfa07ecbd48dc943a74144b2c2c2e8ff8991
A-Amanathullah <132488394+A-Amanathullah@users.noreply.github.com>
2024-05-27 12:45:04 +0530

    name change

--- data_injection.py
+++ data_injection.py
@@ -7,8 +7,14 @@ class IData_injection(ABC):
   def data_inject(self,path):
     pass
 
-class JSON_data_injector(IData_injection):
+class IJSON_data_injector(IData_injection):
   def data_inject(self, path):
     with open (path,"r") as j:
       json_data = json.load(j)
       return json_data
+    
+class ICSV_data_injector(IData_injection):
+  def data_inject(self, path):
+    with open (path,"r") as c:
+      csv_data = json.load(c)
+      return csv_data

0053ac0d243024e19aa5da1f40e9ba310bf29301
A-Amanathullah <132488394+A-Amanathullah@users.noreply.github.com>
2024-05-27 12:31:43 +0530

    name change

--- data_injection.py
+++ data_injection.py
@@ -1,7 +1,12 @@
-form abc import ABC, abstractmethod
+from abc import ABC, abstractmethod
 
 class IData_injection(ABC):
   @abstractmethod
   
   def data_inject(self,path):
     pass
+
+class CSV_data_injector(IData_injection):
+  def data_inject(self, path):
+    with open (path,"r") as csv:
+      

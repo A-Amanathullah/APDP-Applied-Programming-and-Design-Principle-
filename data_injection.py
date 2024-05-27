4555016420f24ab56a16adbb06ed0ab7afaa2016
A-Amanathullah <132488394+A-Amanathullah@users.noreply.github.com>
2024-05-27 12:37:00 +0530

    name change

--- data_injection.py
+++ data_injection.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import JSON as json
 
 class IData_injection(ABC):
   @abstractmethod
@@ -6,7 +7,8 @@ class IData_injection(ABC):
   def data_inject(self,path):
     pass
 
-class CSV_data_injector(IData_injection):
+class JSON_data_injector(IData_injection):
   def data_inject(self, path):
-    with open (path,"r") as csv:
-      
+    with open (path,"r") as j:
+      json_data = json.load(j)
+      return json_data
